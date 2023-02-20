98a596c3802f00a6583b2458fc129b68a9eda5fa
Derrick Omori <omoriderrick@gmail.com>
2023-02-20 15:57:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
