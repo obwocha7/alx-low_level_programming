4cd391e4233c17a636892dd20d7fa97b1575a705
Derrick Omori <omoriderrick@gmail.com>
2023-02-21 09:42:04 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Displays the alphabet
+ * Return: nulll
+ */
+
+void print_alphabet(void)
+
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+}
