76c9b465a1b5bbd78bbde5597bb0a63a69b98ff8
Derrick Omori <omoriderrick@gmail.com>
2023-02-21 07:16:11 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - begins the program
+ * this program prints a value
+ * return - returns 0 if success.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
